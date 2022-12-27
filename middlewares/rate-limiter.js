cd5765e90f19a625f6f13fce87e2c0954d88925a
Ravi Kant Sharma <ravikant.sharma@cybergindia.local>
2022-12-28 01:13:47 +0530

    updated config var names

--- middlewares/rate-limiter.js
+++ middlewares/rate-limiter.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const rateLimit = require('express-rate-limit');
 
 exports.RateLimiter = rateLimit({
-  windowMs: process.env.RATE_LIMIT_WINDOW * 60 * 1000 || 1 * 60 * 1000, // 1 Minute
-  max: process.env.API_RATE_LIMIT, // Limit each IP to 5 create account requests per `window` (here, per hour)
-  message: 'Too many API requests intercepted from this IP, please try again after a minute',
+  windowMs: (parseInt(process.env.RATE_LIMIT_WINDOW) || 1) * 60 * 1000, // 1 Minute
+  max: parseInt(process.env.RATE_LIMIT_PER_WINDOW) || 10, // Limit each IP to 5 create account requests per `window` (here, per hour)
+  message: 'Too many API requests intercepted from this IP. Your API rate limit is exhausted, please try again after a minute',
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
