92ba053654a72ea268609489fe331b00096c86f1
Ravi Kant Sharma <ravikant.sharma@cybergindia.local>
2022-12-28 19:14:14 +0530

    added Basic Auth Secret

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Gateway = require('./routes/gateway');
 const { Cors } = require('./middlewares/cors');
 const { Logger } = require('./middlewares/logger');
 const { RateLimiter } = require('./middlewares/rate-limiter');
-const { Resolver } = require('./middlewares/resolver');
+const { Auth } = require('./middlewares/auth');
 
 //Setting port to run app
 const PORT = process.env.PORT || 8000;
@@ -30,13 +30,13 @@ if (process.env.ENABLE_HTTP_LOGGING) {
     app.use(Logger);
 };
 
-// Enabling Rate Limiting if user OPTS
-if (process.env.ENABLE_HTTP_LOGGING) {
-    app.use(RateLimiter);
+// Enabling Authentication if user OPTS
+if (process.env.AUTHENTICATION === "enabled") {
+    app.use(Auth);
 };
 
-// Enabling Authentication if user OPTS
-if (process.env.ENABLE_AUTHENTICATION) {
+// Enabling Rate Limiting if user OPTS
+if (process.env.ENABLE_HTTP_LOGGING) {
     app.use(RateLimiter);
 };
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,3 +1,27 @@
 exports.Auth = async (req, res, next) => {
-    next();
+    if (process.env.AUTHENTICATION === "enabled") {
+        if (req.headers.authorization) {
+            if (process.env.BASIC_AUTH == "enabled") {
+                if (req.headers.authorization === `Basic ${Buffer.from(`${process.env.BASIC_AUTH_USERNAME}:${process.env.BASIC_AUTH_PASSWORD}`).toString('base64')}`) {
+                    next();
+                } else {
+                    res.status(401).send({
+                        message: "Invalid authorization headeraaaaa"
+                    });
+                }
+            } else if (process.env.JWT_AUTH === "enabled") {
+                //check jwt auth here
+            } else {
+                res.status(401).send({
+                    message: "Invalid authorization headerssssss"
+                });
+            }
+        } else {
+            res.status(401).send({
+                message: "Invalid authorization headercccccccccc"
+            });
+        }
+    } else {
+        next();
+    }
 };
