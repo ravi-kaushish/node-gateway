4c82e44955c7434f65600e8d443a2db03257f679
Ravi Kant Sharma <ravikant.sharma@cybergindia.local>
2022-12-28 20:21:41 +0530

    added ability to auto identitfy auth type

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 exports.Auth = async (req, res, next) => {
     if (process.env.AUTHENTICATION === "enabled") {
         if (req.headers.authorization) {
-            if (process.env.BASIC_AUTH == "enabled") {
+            let auth_type = req.headers.authorization.split(" ")[0];
+            if (process.env.BASIC_AUTH == "enabled" && auth_type=== "Basic") {
                 if (req.headers.authorization === `Basic ${Buffer.from(`${process.env.BASIC_AUTH_USERNAME}:${process.env.BASIC_AUTH_PASSWORD}`).toString('base64')}`) {
                     next();
                 } else {
@@ -11,7 +12,7 @@ exports.Auth = async (req, res, next) => {
                         message: "Invalid authorization header"
                     });
                 }
-            } else if (process.env.JWT_AUTH === "enabled") {
+            } else if (process.env.JWT_AUTH === "enabled" && auth_type=== "Bearer") {
                 jwt.verify(req.headers.authorization.replace("Bearer ", ""), process.env.JWT_AUTH_SIGNATURE, function (err, decoded) {
                     if (err) {
                         res.status(401).send({
